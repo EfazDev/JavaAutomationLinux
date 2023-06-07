dd1f4bc4116ce6fffb2003a72e6602644f0cc381
MegaSquadEfaz <efaz2019@icloud.com>
2023-06-07 18:18:30 -0500

    Online Launcher small update

--- OnlineRunner/JavaAutomationStarter.py
+++ OnlineRunner/JavaAutomationStarter.py
@@ -39,8 +39,8 @@ resp = requests.get(url)
 if resp.status_code == 200:
     print("Finished GET Request, saving script...")
     content = resp.text
-    f = open("ExtenderRunner.py", "w")
-    f.write(content)
+    with open("ExtenderRunner.py", "w", encoding="utf-8") as f:
+        f.write(content)
     print("Finished Writing Script")
     print("Running Script...")
     subprocess.Popen([whichPythonCommand(), 'ExtenderRunner.py'])
